88634adc94c897d59a0ab9ddd8d86f8cfdd533fc
System Administrator <root@juanrod.local>
2017-04-15 10:01:04 -0500

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+   <h2>lovely face ugly city</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
